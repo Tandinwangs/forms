a6734b2bf52bbe1c84bbc49a94445bb78f3dea2d
Tashi Phuntsho <taphin508@gmail.com>
2021-01-07 16:29:41 +0600

    bug fix 4

--- resources/views/admin/rolesandforms.blade.php
+++ resources/views/admin/rolesandforms.blade.php
@@ -17,12 +17,12 @@
 					        <td><span class="badge bg-primary">{{$r->role}}</span></td>
 					        <td>
 					        	@foreach($r->forms as $f)
-					        		<a href="{{ route('unlink_form_path',$f->id) }}" class="bnb-btn-y" data-toggle="tooltip" title="Un-Link the form {{$r->role}} Role">{{ $f->form->form }}</a>
+					        		<a href="{{ route('unlink_form_path',$f->id) }}" class="bnb-btn-y m-2" data-toggle="tooltip" title="Un-Link the form {{$r->role}} Role"><small>{{ $f->form->form }}</small></a>
 					        	@endforeach
 					        </td>
 					        <td class="text-center">
 			        			@foreach($forms as $fm)
-					        		<a href="{{ route('link_form_path',[$r->id,$fm->id]) }}" class="bnb-btn-b" data-toggle="tooltip" title="Link the form to {{$r->role}} Role"><small>{{$fm->form}}</small></a>
+					        		<a href="{{ route('link_form_path',[$r->id,$fm->id]) }}" class="bnb-btn-b m-2" data-toggle="tooltip" title="Link the form to {{$r->role}} Role"><small>{{$fm->form}}</small></a>
 					        	@endforeach
 					        </td>
 					    </tr>
