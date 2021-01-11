e75b4f864df3ec1b6e93f7bfe49866ebb2022cb9
Tashi Phuntsho <taphin508@gmail.com>
2021-01-11 11:18:29 +0600

    bulkupdate bugfix4

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -39,8 +39,10 @@
 			</div>		
 		</div>
 	</div>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> 
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+  	<script src="{{ asset('js/popper.min.js') }}"></script>
+	<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> --> 
 	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 	<script src="{{ asset('js/alert.js') }}"></script>
 	<script src="{{ asset('js/jq.js') }}"></script>
