e6a273941b8c8f6a9979e7ed1eaeb8024c838200
Tashi Phuntsho <taphin508@gmail.com>
2021-01-25 18:37:15 +0600

    FMS Bigfix 3

--- resources/views/forms/forms.blade.php
+++ resources/views/forms/forms.blade.php
@@ -15,6 +15,37 @@
 						</a>
 					</div>
 				@endforeach
+
+				<div class="col-md-6 mb-3">
+					<a href="https://forms.gle/BRGrMQz6Q3tqqrnW6" class="no-decoration">
+						<div class="form-container">
+							<h1>MoneyGram Claim Form</h1>
+							<hr>
+							<h4>This online remittance claim form is brought to you by Bhutan National Bank Ltd.  and the facility is currently made available for BNB account holders only. </h4>
+						</div>
+					</a>
+				</div>
+
+				<div class="col-md-6 mb-3">
+					<a href="https://www.bnb.bt/new-atm-debit-card/" class="no-decoration">
+						<div class="form-container">
+							<h1>Apply for a new RuPay / ATM Debit Card </h1>
+							<hr>
+							<h4>Kindly fill this form if you are applying for RuPay / ATM Debit card for the first time.</h4>
+						</div>
+					</a>
+				</div>
+
+				<div class="col-md-6 mb-3">
+					<a href="https://www.bnb.bt/new-atm-debit-card/" class="no-decoration">
+						<div class="form-container">
+							<h1>Duplicate ATM/Debit Card Online Application</h1>
+							<hr>
+							<h4>Kindly fill this form if you are applying for RuPay / ATM Debit card replacement.</h4>
+						</div>
+					</a>
+				</div>
+
 			</div>
 		</div>
 	</div>
