cce40e5b1518bd4f461f33413d74aaedf720e6a8
LT2022060104 <LT2022060104@bank.bnb>
2023-10-09 17:15:00 +0600

    final

--- database/seeds/DzongkhagTableSeeder.php
+++ database/seeds/DzongkhagTableSeeder.php
@@ -15,16 +15,105 @@ class DzongkhagTableSeeder extends Seeder
     {
         DB::table('branches')->insert([
             [
-                'branch_code' => '1103219',
-                'branch_name' => 'fjad Phodrang Dzongkhag',
+                'branch_code' => '11001',
+                'branch_name' => 'Bumthang Dzongkhag',
                 'category' => 'dzongkhag',
               ],
               [
-                'branch_code' => '113200',
-                'branch_name' => 'djsa Dzongkhag',
+                'branch_code' => '11002',
+                'branch_name' => 'Chhukha Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11003',
+                'branch_name' => 'Dagana Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11004',
+                'branch_name' => 'Gasa Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11005',
+                'branch_name' => 'Haa Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11006',
+                'branch_name' => 'Lhuentse Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11007',
+                'branch_name' => 'Mongar Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11008',
+                'branch_name' => 'Paro Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11009',
+                'branch_name' => 'Pemagatshel Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11010',
+                'branch_name' => 'Punakha Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11011',
+                'branch_name' => 'Samdrup Jongkhar Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11012',
+                'branch_name' => 'Samtse Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11013',
+                'branch_name' => 'Sarpang Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11014',
+                'branch_name' => 'Thimphu Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11015',
+                'branch_name' => 'Trashigang Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11016',
+                'branch_name' => 'Trashiyangtse Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11017',
+                'branch_name' => 'Trongsa Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11018',
+                'branch_name' => 'Tsirang Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11019',
+                'branch_name' => 'Wangdue Phodrang Dzongkhag',
+                'category' => 'dzongkhag',
+              ],
+              [
+                'branch_code' => '11020',
+                'branch_name' => 'Zhemgang Dzongkhag',
                 'category' => 'dzongkhag',
               ]
-            // Add more data as needed
         ]);
     }
 }
